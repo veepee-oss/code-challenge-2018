6749eb49c5035fd877942f6971f30869611be93d
David Amigo <david.amigo@privalia.com>
2020-10-06 13:15:55 +0200

    Add scripts to backup and restore the mysql database extracting the file from the mysql container to the host

--- docker/includes.sh
+++ docker/includes.sh
@@ -6,6 +6,8 @@ DOCKER_SERVER="docker_cc18_server"
 DOCKER_MYSQL="docker_cc18_mysql"
 DOCKER_MYSQL_USER="cc18_user"
 DOCKER_MYSQL_PASSWORD="cc18_pass"
+DOCKER_MYSQL_DATABASE="cc18_db"
+DOCKER_MYSQL_BACKUP_FILE="${DOCKER_MYSQL_DATABASE}.bak.sql"
 
 APP_PHP="/usr/local/bin/php"
 APP_COMPOSER="/usr/local/bin/composer"
