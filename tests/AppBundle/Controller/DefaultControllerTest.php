15527dc385db321d0357f54a16ec4f8fee21bdf8
David Amigo <david.amigo@privalia.com>
2020-10-15 16:44:14 +0200

    Add debugging options to Dockerfile (not in production)

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -12,7 +12,7 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), $client->getResponse()->getContent());
         $this->assertContains('Code Challenge', $crawler->filter('.jumbotron .container p')->text());
         $this->assertContains('See the rules', $crawler->filter('.jumbotron .container a.btn')->text());
     }
